a17bc2c531ab7c6e94a7a4f70324790e87a3366a
s13146-pj <56793702+s13146@users.noreply.github.com>
2022-10-19 19:28:14 +0200

    Test finish

--- 1/src/main/java/com/example/lab1/Calculator.java
+++ 1/src/main/java/com/example/lab1/Calculator.java
@@ -1,49 +1,87 @@
 package com.example.lab1;
 
-
 import java.util.Scanner;
 
 public class Calculator {
     private static Scanner scanner;
 
-    public int Dodawanie(int num1, int num2) {
-        return num2 + num1;
+    public static int suma(int num1, int num2) {
+        return num1 + num2;  //ta metoda zwraca sumę dwóch parametrów
     }
 
-    public void odejmowanie() {
-        //   output = operand1 - operand2;
+    public static int odejmowanie(int num1, int num2) {
+        return num1 - num2;
     }
 
-    public void mnozenie() {
-        // output = operand1 * operand2;
+    public static int mnozenie(int num1, int num2) {
+        return num1 * num2;  //ta metoda zwraca sumę dwóch parametrów
     }
 
-    public void dzielenie() {
-        //   output = operand1 / operand2;
+    public static int dziel1(int num1, int num2) {
+        return num1 / num2;
+    }
+
+    public static String dzielenie(int num1, int num2) {
+        String result = "";
+        if (num2 != 0) System.out.println("Dzielenie: " + (num1 / num2));
+        else if (num2 == 0)
+            System.out.println("Nie dzielimy przez zero");
+        return result = "Nie dzielimy przez zero";
+
     }
 
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        scanner = new Scanner(System.in); //for the input from user
-        System.out.println("Podaj swoj wiek:");
-        int wiek = scanner.nextInt(); //take first number as an input
-        //System.out.println("Enter the second number:");
-        // int num2 = scanner.nextInt(); //take second number as an input
+        System.out.println("SPRAWDZENIE TEMPERATURY");
+        System.out.println("Podaj temperature");
 
-        //wynik= dodawanie(num1,num2);
-        //int  wynik = Dodawanie(num1,num2);
-        //  System.out.println(
-        //         "wynik dodawania " + wynik
-        //  );
-        if (wiek<18) System.out.println("Nie jesteś pełnoletni ");
-        else if (wiek >18 && wiek <21 ) System.out.println("Jesteś pełnoletni ale nie mozesz");
-        else System.out.println("Jestes stary");
+        int temperatura = scanner.nextInt();
+
+
+        isTemperature(temperatura);
+
+
+        System.out.println("KALKULATOR");
+        System.out.println("Podaj pierwsza liczbe");
+        int num1 = scanner.nextInt();
+        System.out.println("Podaj drugą liczbe");
+        int num2 = scanner.nextInt();
+        System.out.println("Są dwie liczby: " + num1 + " i " + num2);
+
+        int wynik_s = suma(num1, num2);
+        System.out.println("Dodawnia: " + suma(num1, num2));
+        int wynik_o = odejmowanie(num1, num2);
+        System.out.println("Odjemowania: " + odejmowanie(num1, num2));
+        int wynik_m = mnozenie(num1, num2);
+        System.out.println("Mnozenie: " + mnozenie(num1, num2));
+        dzielenie(num1, num2);
+
+        if (num2 == 0)
+            System.out.println("Nie dzielimy przez zero");
+        else System.out.println("Dzielenie: " + dziel1(num1, num2));
+
+
     }
 
+
+    public static String isTemperature(int temperatura) {
+        String temp = "";
+        if (temperatura > 0) {
+            temp = "Temperatura jest dodatnia.";
+            System.out.println(temp);
+        } else {
+            temp = "Temperatura jest ujemna.";
+            System.out.println(temp);
+        }
+        return temp;
+
+    }
 }
 
-//   System.out.println("Output result=" + output);
+
+
 
 
 
