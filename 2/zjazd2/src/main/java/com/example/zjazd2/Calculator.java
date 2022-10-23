bcaa78b5eec66d6a8582a887a0638dba9bd81c33
s13146 <s13146@pjwstk.edu.pl>
2022-10-23 13:34:52 +0200

    first commit

---
+++ 2/zjazd2/src/main/java/com/example/zjazd2/Calculator.java
@@ -0,0 +1,92 @@
+package com.example.zjazd2;
+import java.util.Scanner;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Calculator {
+
+
+    private static Scanner scanner;
+
+    public static int suma(int num1, int num2) {
+        return num1 + num2;  //ta metoda zwraca sumę dwóch parametrów
+    }
+
+    public static int odejmowanie(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public static int mnozenie(int num1, int num2) {
+        return num1 * num2;  //ta metoda zwraca sumę dwóch parametrów
+    }
+
+    public static int dziel1(int num1, int num2) {
+        return num1 / num2;
+    }
+
+    public static String dzielenie(int num1, int num2) {
+        String result = "";
+        if (num2 != 0) System.out.println("Dzielenie: " + (num1 / num2));
+        else if (num2 == 0)
+            System.out.println("Nie dzielimy przez zero");
+        return result = "Nie dzielimy przez zero";
+
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("SPRAWDZENIE TEMPERATURY");
+        System.out.println("Podaj temperature");
+
+        int temperatura = scanner.nextInt();
+
+
+        isTemperature(temperatura);
+
+
+        System.out.println("KALKULATOR");
+        System.out.println("Podaj pierwsza liczbe");
+        int num1 = scanner.nextInt();
+        System.out.println("Podaj drugą liczbe");
+        int num2 = scanner.nextInt();
+        System.out.println("Są dwie liczby: " + num1 + " i " + num2);
+
+        int wynik_s = suma(num1, num2);
+        System.out.println("Dodawnia: " + suma(num1, num2));
+        int wynik_o = odejmowanie(num1, num2);
+        System.out.println("Odjemowania: " + odejmowanie(num1, num2));
+        int wynik_m = mnozenie(num1, num2);
+        System.out.println("Mnozenie: " + mnozenie(num1, num2));
+        dzielenie(num1, num2);
+
+        if (num2 == 0)
+            System.out.println("Nie dzielimy przez zero");
+        else System.out.println("Dzielenie: " + dziel1(num1, num2));
+
+
+    }
+
+
+    public static String isTemperature(int temperatura) {
+        String temp = "";
+        if (temperatura > 0) {
+            temp = "Temperatura jest dodatnia.";
+            System.out.println(temp);
+        } else {
+            temp = "Temperatura jest ujemna.";
+            System.out.println(temp);
+        }
+        return temp;
+
+    }
+}
+
+
+
+
+
+
---
+++ 2/zjazd2/src/test/java/com/example/zjazd2/CalculatorTest.java
@@ -0,0 +1,102 @@
+package com.example.zjazd2;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CalculatorTest {
+
+    Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
+    @AfterEach
+    void tearDown() {
+        calculator = null;
+
+    }
+
+    @Test
+    public void test1() {
+        int result = Calculator.suma(2, 2);
+        assertEquals(4, result);
+        System.out.println("Test1");
+    }
+
+    @Test
+    public void test2() {
+        int result = Calculator.odejmowanie(2, 2);
+        assertEquals(0, result);
+        System.out.println("Test2");
+    }
+
+    @Test
+    public void test3() {
+        int result = Calculator.mnozenie(2, 2);
+        assertEquals(4, result);
+        System.out.println("Test3");
+    }
+
+    @Test
+    public void test4() {
+        String result = Calculator.dzielenie(2, 0);
+        assertEquals("Nie dzielimy przez zero", result);
+        System.out.println("Test4");
+    }
+
+    @Test
+    public void test5() {
+        assertThrows(ArithmeticException.class, () -> calculator.dziel1(10, 0));
+        System.out.println("Test5");
+    }
+
+    @Test
+    public void test6() {
+        String result = Calculator.isTemperature(20);
+        assertNotEquals("Temperatura jest ujemna.", result);
+        System.out.println("Test6");
+    }
+
+    @Test
+    public void test7() {
+        String result = Calculator.isTemperature(-3);
+        assertNotEquals("Temperatura jest dodatnia", result);
+        System.out.println("Test7");
+    }
+
+    @Test
+    public void test8() {
+        String result = Calculator.isTemperature(20);
+        assertEquals("Temperatura jest dodatnia.", result);
+        System.out.println("Test8");
+    }
+
+    @Test
+    public void test9() {
+        String result = Calculator.isTemperature(-10);
+        assertEquals("Temperatura jest ujemna.", result);
+        System.out.println("Test9");
+    }
+
+    @Test
+    public void test10() {
+        String result = Calculator.dzielenie(4, 0);
+        assertEquals("Nie dzielimy przez zero", result);
+        System.out.println("Test9");
+    }
+
+    @Test
+    public void test11() {
+        String result = Calculator.dzielenie(0, 4);
+        assertNotEquals("Nie dzielimy przez zero", result);
+        System.out.println("Test9");
+    }
+
+}
